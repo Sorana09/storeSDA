5dc9c3a783e887c825661467cf0a069e29e26542
Sorana09 <sorananeculaed@gmail.com>
2024-05-11 11:05:15 +0300

    list products

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,19 +5,45 @@ import org.example.provider.SessionFactoryProvider;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        SessionFactory sessionFactory= SessionFactoryProvider.provideSessionFactory();
-        Session session=sessionFactory.openSession();
-        Transaction t=session.beginTransaction();
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("0. Iesire");
+            System.out.println("1. Vizualizare produse");
+            System.out.println("2. Adaugare produs");
+            System.out.println("3. Stergere produs");
+            System.out.println("4. Editare produs");
+            int number = scanner.nextInt();
+            if (number == 0)
+                break;
+            else
+                switch (number) {
+                    case 1: {
+                        System.out.println(viewProducts());
+                        break;
+                    }
+                    default:
+                        System.out.println("Nu exista optiunea. Va rugam reincercati.");
+                        break;
+                }
+        }
+
+    }
+
+    private static void addProduct() {
+        SessionFactory sessionFactory = SessionFactoryProvider.provideSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction t = session.beginTransaction();
 
         Product p = new Product();
         p.setName("Mere");
@@ -28,6 +54,14 @@ public class App
         t.commit();
 
         sessionFactory.close();
-
     }
+
+   private static List<Product> viewProducts(){
+       SessionFactory sessionFactory = SessionFactoryProvider.provideSessionFactory();
+       Session session = sessionFactory.openSession();
+//       Query<Product> query = session.createNativeQuery("SELECT * FROM product",Product.class);
+       Query<Product> query = session.createQuery("SELECT p FROM Product p",Product.class);
+       return query.getResultList();
+   }
+
 }
