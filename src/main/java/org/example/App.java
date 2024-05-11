189a61d40f4a6cd12248b24c1bd68504e3099e7b
Sorana09 <sorananeculaed@gmail.com>
2024-05-11 10:30:11 +0300

    create Product

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.example.model.Product;
+import org.example.provider.SessionFactoryProvider;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SessionFactory sessionFactory= SessionFactoryProvider.provideSessionFactory();
+        Session session=sessionFactory.openSession();
+        Transaction t=session.beginTransaction();
+
+        Product p = new Product();
+        p.setName("Mere");
+        p.setPrice(12.5f);
+        p.setStock(50);
+        session.merge(p);
+        session.persist(p);
+        t.commit();
+
+        sessionFactory.close();
+
     }
 }
---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,24 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private float price;
+    private int stock;
+
+}
