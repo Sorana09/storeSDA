6c527410f31ad4e05a5155cb6948e1c413b2cda9
Sorana09 <sorananeculaed@gmail.com>
2024-05-31 15:14:53 +0300

    merge backup into master

--- src/main/java/org/example/utils/Utils.java
+++ src/main/java/org/example/utils/Utils.java
@@ -11,12 +11,36 @@ public class Utils {
                 .forEach(System.out::println);
     }
 
-    public static Product printProductsWithIndexes(List<Product> productList, Scanner scanner) {
+    public static Product chooseProduct(List<Product> productList, Scanner scanner) {
         for (int i = 0; i < productList.size(); i++) {
-            System.out.println((i + 1) + ". " + productList.get(i));
+            System.out.println((i + 1) + ". " + productList.get(i).getName() + " " + productList.get(i).getPrice() + " " + productList.get(i).getStock() + "buc");
         }
-        System.out.print("Choose product: ");
-        int index = scanner.nextInt();
+
+//        int index = scanner.nextInt();
+        int index = Utils.readInt(scanner, "Alegeti un produs", 1, productList.size());
         return productList.get(index - 1);
+    }
+
+    public static int readInt(Scanner scanner, String label, Integer min, Integer max) {
+        System.out.print(label + " (" + min + " - " + max + "): ");
+        int quantity = scanner.nextInt();
+        while ((min != null && quantity < min) || (max != null && quantity > max)) {
+            Utils.printError(ConstantsRomanian.INVALID_INPUT);
+            System.out.print(label + ": ");
+            quantity = scanner.nextInt();
+        }
+        return quantity;
+    }
+
+    public static void printError(String message) {
+        // https://www.compart.com/en/unicode/U+1F534
+        // https://www.compart.com/en/unicode/search?q=red#characters
+        System.out.println("\uD83D\uDD34 " + message);
+    }
+
+    public static void printSuccess(String message) {
+        // https://www.compart.com/en/unicode/U+1F7E2
+        // https://www.compart.com/en/unicode/search?q=green#characters
+        System.out.println("\uD83D\uDFE2 " + message);
     }
 }
